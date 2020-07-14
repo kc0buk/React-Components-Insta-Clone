8498b19c82c3c9954ea60679841e2e997314912c
Jeremiah Trnka <jeremiah@jeremiahs-mbp.lan>
2020-07-14 16:52:19 -0700

    Updated App.js with revised code to enable search.

--- src/App.js
+++ src/App.js
@@ -17,8 +17,19 @@ const App = () => {
   // Create a state called 'posts' to hold the list of posts, initializing to dummyData.
   // To make the search bar work (which is stretch) we'd need another state to hold the search term.
   const [posts, setPosts] = useState(data)
-  const [searchTerm, setSearch] = useState('')
-
+  const [searchTerm, setSearch] = useState(data)
+  // console.log(post.comments)
+  const filterPosts = (e) => {
+    console.log(e)
+    // setSearch(posts.filter(post => 
+    //   post.comments.forEach(term => {
+    //     term.text.includes(e) && console.log(term)
+    //   })
+    // ))
+    setSearch(posts.filter(item => item.username.includes(e)))
+    
+  }
+// console.log(searchTerm)
   const likePost = postId => {
     // This function is passed into nested components using props, to allow them to update application state.
     // It takes a post id as its only argument. The idea is to increase the 'likes' count of the post with the given `id`.
@@ -26,7 +37,7 @@ const App = () => {
     // The callback passed into `posts.map()` performs the following logic:
     //  - if the `id` of the post matches `postId`, return a new post object containing an increased 'likes' count.
     //  - otherwise just return the post object unchanged.
-    setPosts(posts.map(post => {
+    setSearch(searchTerm.map(post => {
       if(post.id === postId) {
         return { ...post, likes: post.likes + 1 }
       }
@@ -37,8 +48,8 @@ const App = () => {
   return (
     <div className="App">
       {/* Add SearchBar and Posts here to render them */}
-      <SearchBar />
-      <Posts posts={posts} likePost={likePost} />
+      <SearchBar filterPosts={filterPosts} searchTerm={searchTerm}/>
+      <Posts posts={searchTerm} likePost={likePost} />
       {/* Check the implementation of each component, to see what props they require, if any! */}
     </div>
   );
